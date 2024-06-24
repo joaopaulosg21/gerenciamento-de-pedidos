00c8cabb9f9f333c0ae92f07c76223c9bbafc941
joaopaulo <joaopaulosg17@gmail.com>
2024-06-24 15:25:24 -0300

    feat: Adicionado metodo para enviar a criação de novas orders para a queue que sera consumida pelo service de product [ORDER]

--- orders-service/src/main/java/projeto/services/orders_service/domain/producer/MessageProducer.java
+++ orders-service/src/main/java/projeto/services/orders_service/domain/producer/MessageProducer.java
@@ -1,8 +1,10 @@
 package projeto.services.orders_service.domain.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import projeto.services.orders_service.domain.order.OrderDetailsDTO;
 
@@ -14,7 +16,18 @@ public class MessageProducer {
 
     private final ObjectMapper mapper;
 
+    @Value("${orders.exchange}")
+    private String ordersExchange;
+
+    @Value("${orders.key}")
+    private String ordersKey;
+
     public void sendToOrderQueue(OrderDetailsDTO details) {
-        System.out.println(details + " Enviado para a fila");
+        try {
+            var json = mapper.writeValueAsString(details);
+            amqpTemplate.convertAndSend(ordersExchange, ordersKey, json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
